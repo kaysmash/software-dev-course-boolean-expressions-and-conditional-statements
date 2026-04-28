79e6f97d3a105d348d9f67315d8cb52c59f3f431
Kristen Smith <kristen10.ks@gmail.com>
2026-04-28 15:59:01 -0500

    push assignment

--- boolean-expressions-and-conditional-statements.js
+++ boolean-expressions-and-conditional-statements.js
@@ -1,52 +1,48 @@
-/*
-
-Objective:
-You will practice creating and combining boolean expressions
-to drive logic and outcomes in you program.
-
-Instructions:
-If you are not familiar with the concept of a text-based adventure game,
-let's set the scene...
-Example: "You wake up in a dark forest. There are two paths ahead of you:
-one leading to the mountains and one to a village.
-Your choices will determine your fate!"
-
-Define the Requirements: You must:
-  - Write conditional statements to handle player choices.
-  - Use boolean expressions to combine multiple conditions.
-  - Include at least one use of logical operators (&&, ||, !).
-
-Starter Code:
-  - Run the following command in your terminal to install the readline-sync module:
-    npm install readline-sync
-
-Paste the following code into your editor:
-
-*/
-
 const readline = require('readline-sync');
 
-const hasTorch = true;
-const hasMap = false;
+console.log("Your new friend wants to go out to eat. To find out where to go, they ask if you have a nut allergy.");
+const answer = readline.question("Do you have a nut allergy? (yes/no)");
 
-console.log("You see two paths: one leads to the mountains, the other to the village.");
-const choice = readline.question("Do you go to the 'mountains' or the 'village'?");
+if (answer === "yes") {
+  console.log("Your friend suggests going to a restaurant that serves Mexican food.");
 
-if (choice === "mountains" && hasTorch) {
-  console.log("You safely navigate through the dark mountains.");
-} else if (choice === "mountains" && !hasTorch) {
-  console.log("It's too dark to proceed. You decide to turn back.");
-} else if (choice === "village" || hasMap) {
-  console.log("You find your way to the village.");
+  const yesDrinkAlcohol = true;
+  const noDrinkAlcohol = false;
+  const maybeDrinkAlcohol = true;
+
+  console.log("Your new friend also wants to know if you drink alcohol.");
+  const answer2 = readline.question("Do you drink alcohol? (yes/no/maybe)");
+
+  if (answer2 === "yes" && yesDrinkAlcohol) {
+    console.log("Your friend suggests going to Los Patio.");
+  } else if (answer2 === "no" && noDrinkAlcohol) {
+    console.log("Your friend suggests going to the taco cart.");
+  } else if (answer2 === "maybe" && maybeDrinkAlcohol) {
+    console.log("Your friend suggests going to a restaurant that serves both alcoholic and non-alcoholic beverages.");
+  } else {
+    console.log("Your friend suggests a casual cafe for your meal.");
+  }
+
+} else if (answer === "no") {
+  console.log("Your friend suggests going to a restaurant that serves Thai food.");
+
+  const yesDrinkAlcoholT = true;
+  const noDrinkAlcoholT = false;
+  const maybeDrinkAlcoholT = true;
+
+  console.log("Your new friend also wants to know if you drink alcohol.");
+  const answer2 = readline.question("Do you drink alcohol? (yes/no/maybe)");
+
+  if (answer2 === "yes" && yesDrinkAlcoholT) {
+    console.log("Your friend suggests going to Thai Me Down.");
+  } else if (answer2 === "no" && noDrinkAlcoholT) {
+    console.log("Your friend suggests going to My Thai.");
+  } else if (answer2 === "maybe" && maybeDrinkAlcoholT) {
+    console.log("Your friend suggests going to a restaurant that serves both alcoholic and non-alcoholic beverages.");
+  } else {
+    console.log("Your friend suggests a restaurant with lots of drink options.");
+  }
+
 } else {
-  console.log("You get lost and wander aimlessly.");
+  console.log("Your friend is not sure what to suggest because the answer wasn't yes or no.");
 }
-
-/* 
-
-Add Customization and expand the game:
-  - Add more choices and scenarios.
-  - Include additional items (e.g., a sword, a compass).
-  - Use nested conditionals and logical operators to create complex outcomes.
-
-*/
